971d61a89bac3ae94366c0494edd1c330534d40c
wiiiky <wiiiky@yeah.net>
2015-01-01 15:12:44 +0800

    filter

--- model.py
+++ model.py
@@ -28,17 +28,18 @@ if not DB_NAME or not DB_USER or not DB_PASSWORD or not DB_HOST:
     raise Exception('MySQL setting not found')
 
 
-class BaseModel:
+class BaseModel(object):
     """docstring for BaseModel"""
 
     pk = IntegerField(primary_key = True,auto_increment = True)
 
     def __init__(self):
         self._fields = []   # 记录列名以及类型
-        self._values = {}
 
         bases = self.__class__.__bases__
-        while bases:
+        flag = True
+        while flag:
+            flag = False
             for b in bases:
                 if issubclass(b, BaseModel):
                     for k, v in b.__dict__.items():
@@ -46,6 +47,7 @@ class BaseModel:
                             self._fields.append((k,v))
 
                     bases = b.__bases__
+                    flag = True
                     break
 
         for k, v in self.__class__.__dict__.items():
@@ -57,16 +59,38 @@ class BaseModel:
             self[k] = v.default_value()
 
     def __setitem__(self, k, v):
-        self._values[k] = v
+        self.__dict__[k] = v
 
     def __getitem__(self, k):
-        if self._values.has_key(k):
-            return self._values[k]
-        return None
+        return self[k]
 
     def filter(self, **kwargs):
-        pass
+        statement = 'select '
+        fields = []
+        for k, v in self._fields:
+            fields.append(k)
 
+        statement += ','.join(fields) + ' from ' + self.__class__.__name__
+
+        args = []
+        where = None
+        if len(kwargs)>0:
+            where = []
+            for k, v in kwargs.items():
+                where.append('%s = %%s' % k)
+                args.append(v)
+        if where:
+            statement += ' where ' + ' and '.join(where)
+
+        rows = BaseModel.db_execute(statement,args)
+        l = []
+        for row in rows:
+            c = self.__class__()
+            for i in range(0, len(row)):
+                c[self._fields[i][0]] = row[i]
+            l.append(c)
+
+        return l
 
     def drop(self):
         statement = 'drop table if exists %s' % self.__class__.__name__
